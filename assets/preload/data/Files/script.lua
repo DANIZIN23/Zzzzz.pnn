38d13d38f30fb64d0d914e03a5ffe4fc78ca07e3
Danibomfin <98532797+Danibomfin@users.noreply.github.com>
2022-07-30 22:40:49 -0300

    Delete assets/preload/data/Files directory

--- assets/preload/data/Files/script.lua
+++
@@ -1,9 +0,0 @@
-function onCreate()
-
-    setProperty('boyfriend.visible', false) --false if you dont want him to be visible
-
-    setProperty('gf.visible', false) --true if you dont want gf to be visible
-
-    setProperty('dad.visible', true) --true if u want the opponent to be visible
-
-end
